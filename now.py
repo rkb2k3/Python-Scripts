c1a86bfe2a774db42902198b14c628d4db8f5cd9
thonking <rkb2k3@protonmail.ch>
2017-05-31 20:04:01 +0100

    Added a comment

--- now.py
+++ now.py
@@ -2,3 +2,6 @@
 from datetime import datetime
 now = datetime.now()
 print("\033[1;42m%s/%s/%s | %s:%s:%s\033[1;m") % (now.day, now.month, now.year, now.hour, now.minute, now.second)
+
+
+# By default, the script outputs the day before the month. Feel free to change this.
