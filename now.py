7e6f23c1509d1de9baec32fb5d37fa3cd6fb676e
thonking <rkb2k3@protonmail.ch>
2017-06-01 16:20:12 +0100

    Changed color of outputted text

--- now.py
+++ now.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from datetime import datetime
 now = datetime.now()
-print("\033[1;42m%s/%s/%s | %s:%s:%s\033[1;m") % (now.day, now.month, now.year, now.hour, now.minute, now.second)
+print("\033[1;92m%s/%s/%s | %s:%s:%s\033[1;m") % (now.day, now.month, now.year, now.hour, now.minute, now.second)
 
 
 # By default, the script outputs the day before the month. Feel free to change this.
